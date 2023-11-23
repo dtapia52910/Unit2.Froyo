e07801d01cc3316a12a147425af6fd4cf3a48ed8
dtapia52910 <dtapia52910@gmail.com>
2023-11-22 16:43:04 -0800

    froyo resubmission, many thanks to  Dubin

--- Unit2.Froyo.js
+++ Unit2.Froyo.js
@@ -1,35 +1,25 @@
 
 function promptForFlavors() {
-    return prompt("type strawberry, vanilla, or coffee to see how many were purchased today!");
-}
-const froyo = [
-    {
-      flavor: "strawberry",
-      qty: "1"
-    },
-    {
-      flavor: "vanilla",
-      qty: "3"
-    },
-    {
-      flavor: "coffee",
-      qty: "2"
-    }
-  ]
-
-function displayFlavorQty(flavor) {
-    const foundFlavor = froyo.find(item => item.flavor.toLowerCase() === flavor.toLowerCase());
-
-    if (foundFlavor) {
-        alert(`Today, ${foundFlavor.qty} ${foundFlavor.flavor} froyo(s) were purchased.`);
-    } else {
-        alert(`No information available for ${flavor} flavor.`);
-    }
+    const userInputString = prompt("type a flavor of ice cream to see how many were purchased today!", "strawberry,strawberry,vanilla,vanilla,coffee");
+    const stringArray = userInputString.split(",");
+    return stringArray
 }
 
+const froyoObj = {
+  vanilla:0, 
+  strawberry:0,
+  coffee:0
+}
+
+function displayFlavorQty(flavors) {
+  for(let i=0; i<flavors.length; i++)
+
+  { froyoObj[flavors[i]] = froyoObj[flavors[i]] + 1
+  }
+  console.log (froyoObj)
+}
 
 const userFlavorInput = promptForFlavors();
-
 
 displayFlavorQty(userFlavorInput);
 
